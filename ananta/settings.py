d4b2fd76519e90e30451f68caa7c0405eaa97aaa
iamrjrobin <iamrjrobin@hotmail.com>
2021-03-09 13:51:29 +0600

    debug false to true

--- ananta/settings.py
+++ ananta/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'if8a!eqg6szbyr!5o86b%g=&(p&%k5vcsp#)2gk03b*5-x!$i-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['movie-name-predictor-of-ananta.herokuapp.com',
                 'localhost',
